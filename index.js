6148949faa56ffeef0d95f53587e469045558e6e
Jon Maddox <jon@jonmaddox.com>
2018-07-17 10:37:52 -0400

    support console.warn and console.error

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ const logger = require('./lib/logger');
 (function(){
     var oldLog = console.log;
     console.log = function () {
-      logger.log(arguments)
+      logger.info(arguments)
       oldLog.apply(console, arguments)
+    };
+
+    var oldWarn = console.warn;
+    console.warn = function () {
+      logger.warn(arguments)
+      oldWarn.apply(console, arguments)
+    };
+
+    var oldError = console.error;
+    console.error = function () {
+      logger.error(arguments)
+      oldError.apply(console, arguments)
     };
 })();
